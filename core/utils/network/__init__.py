90e1e2380aa1bcf6a919a34de2ee1285e8335376
Aswanth Vc <aswanth.abam@gmail.com>
2025-09-19 02:58:49 +0530

    feat(network): added network module and get_client_ip function

--- core/utils/network/__init__.py
+++ core/utils/network/__init__.py
@@ -1,11 +1,20 @@
+import ipaddress
 from fastapi import Request
 
 
 async def get_client_ip(request: Request):
     forwarded_for = request.headers.get("x-forwarded-for")
     if forwarded_for:
-        # X-Forwarded-For can be a list of IPs. Take the first one (the original client).
         ip = forwarded_for.split(",")[0].strip()
     else:
         ip = request.client.host if request.client else None
+
+    # Normalize to IPv4 if possible
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+        if ip_obj.version == 6 and ip_obj.ipv4_mapped:
+            ip = str(ip_obj.ipv4_mapped)
+    except ValueError:
+        pass  # not a valid IP, just return as-is
+
     return ip
